32b8b49937dacbb2926709aaf274613a2353ed12
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-10-14 11:09:22 -0700

    amenity file added to the package

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that contains the Amenity class"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """class Amenity that inherits from BaseModel"""
+    name = ""
